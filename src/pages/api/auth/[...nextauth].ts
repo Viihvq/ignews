e0cc0109238caaaa034ef682d2f2955ed58ffbba
Viihvq <Viihvq>
2022-01-27 22:11:34 -0300

    Saving email (FaunaDB)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import GithubProviders from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
 
@@ -14,4 +18,41 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks:{
+    async signIn({user, account, profile}){
+      const { email } = user;
+
+      try{ //Evita o login se a inserção der errado
+        await fauna.query( //inserção no banco
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'), //index criado no fauna
+                  q.Casefold(user.email) //padroniza o email
+                )
+              )
+            ),//faça
+            q.Create(
+              q.Collection('users'),
+              { data: {email}}
+            ), //senão
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'), 
+                q.Casefold(user.email) 
+              )
+            )
+          )
+        )
+        
+        return true; 
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+      
+      console.log(user);
+    },
+  }
 })
